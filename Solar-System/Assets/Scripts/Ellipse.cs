c5524428ef3a7c8eb1301e2aff8ca83bd571006d
orxanigidov <iorxan1995@gmail.com>
2020-07-02 18:39:32 +0400

    Initial commit

---
+++ Solar-System/Assets/Scripts/Ellipse.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Ellipse : MonoBehaviour
+{
+    public float xAxis;
+    public float yAxis;
+
+    public Ellipse(float xAxis, float yAxis)
+    {
+        this.xAxis = xAxis;
+        this.yAxis = yAxis;
+    }
+
+    public Vector2 Evaluate(float t)
+    {
+        float angel = Mathf.Deg2Rad * 360f * t;
+        float x = Mathf.Sin(angel) * xAxis;
+        float y = Mathf.Cos(angel) * yAxis;
+        return new Vector2(x, y);
+    }
+}
---
+++ Solar-System/Assets/Scripts/Rotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public float speed;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.up, speed * Time.deltaTime);
+        transform.Rotate(Vector3.up * speed * Time.deltaTime, Space.Self);
+    }
+}
